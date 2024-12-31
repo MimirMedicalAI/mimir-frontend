bffd39af2c275ac526acfd7c51b1444b8de3f44e
arnoclaude <arno.claude@googlemail.com>
2024-12-31 14:35:58 +0100

    some minor restructure

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import Home from './pages/Home.tsx'
+import Home from './pages/Home/Home.tsx'
 import {BrowserRouter, Route, Routes} from "react-router";
 import Layout from "@/components/layout.tsx";
-import Patients from "@/pages/Patients.tsx";
-import MedAIAssistant from "@/pages/MedAIAssistant.tsx";
-import PatientCommunication from "@/pages/PatientCommunication.tsx";
+import Patients from "@/pages/Patients/Patients.tsx";
+import MedAIAssistant from "@/pages/MedAIAssistant/MedAIAssistant.tsx";
+import PatientCommunication from "@/pages/PatientCommunication/PatientCommunication.tsx";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
